c400d4355ca3bcc3245ce414e10d0242d310a69b
11256036-oss <11256036@ntub.edu.tw>
2026-01-05 16:00:58 +0800

    Delete course directory

--- course/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Course, Enrollment, Comment
-
-admin.site.register(Profile)
-admin.site.register(Course)
-admin.site.register(Enrollment)
-admin.site.register(Comment)
